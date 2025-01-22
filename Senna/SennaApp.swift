5530f097f2fa17f8f33d7d3b6ab2bab62fb17624
Jason Zhu <55606978+jasonmeme@users.noreply.github.com>
2025-01-22 18:19:23 -0500

    Initial Commit

---
+++ Senna/SennaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SennaApp.swift
+//  Senna
+//
+//  Created by Jason Zhu on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SennaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SennaTests/SennaTests.swift
@@ -0,0 +1,17 @@
+//
+//  SennaTests.swift
+//  SennaTests
+//
+//  Created by Jason Zhu on 1/22/25.
+//
+
+import Testing
+@testable import Senna
+
+struct SennaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
